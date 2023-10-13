3139907aa4fd810235fcccd7b993a3887797cf26
Lippay Balazs <lippaybalazs@gmail.com>
2023-10-13 18:31:17 +0300

    added time left display

--- SleepTimer.py
+++ SleepTimer.py
@@ -1,5 +1,11 @@
 import eel
 import subprocess
+import tempfile
+import os
+import json
+import time
+
+TEMP_FILE_PATH = os.path.join(tempfile.gettempdir(), "sleep_timer.json")
 
 
 @eel.expose
@@ -8,6 +14,15 @@ def set_shutdown_timer(seconds: int) -> None:
         "C:\\Windows\\System32\\shutdown.exe -s -t " + str(seconds) + " -f",
         shell=True
     ).wait()
+    with open(TEMP_FILE_PATH, 'w') as file:
+        json.dump(
+            {
+                "set time": int(time.time()),
+                "amount": seconds
+            },
+            file,
+            indent=4
+        )
 
 
 @eel.expose
@@ -16,7 +31,27 @@ def cancel_shutdown_timer() -> None:
         "C:\\Windows\\System32\\shutdown.exe -a",
         shell=True
     ).wait()
+    try:
+        os.remove(TEMP_FILE_PATH)
+    except Exception:
+        pass
 
 
-eel.init('web')
-eel.start('index.html', size=(300, 400), port=0)
+@eel.expose
+def get_shutdown_time_left() -> int:
+    try:
+        with open(TEMP_FILE_PATH) as file:
+            data = json.load(file)
+            now = time.time()
+            return data["amount"] + data["set time"] - int(now)
+    except Exception:
+        try:
+            os.remove(TEMP_FILE_PATH)
+        except Exception:
+            pass
+        return -1
+
+
+if __name__ == "__main__":
+    eel.init('web')
+    eel.start('index.html', size=(300, 450), port=0)
--- web/index.js
+++ web/index.js
@@ -1,8 +1,37 @@
-async function set_shutdown_timer(seconds) {
+var ui_input;
+
+async function SetShutdownTimer(seconds) {
     await eel.cancel_shutdown_timer()();
-    await eel.set_shutdown_timer(seconds)();
+    await eel.set_shutdown_timer(seconds)(() => GetTimeLeft());
 }
 
-async function cancel_shutdown_timer() {
-    await eel.cancel_shutdown_timer()();
+async function CancelShutdownTimer() {
+    await eel.cancel_shutdown_timer()(() => GetTimeLeft());
+}
+
+async function OnLoad() {
+    ui_input = document.getElementById("ui_input");
+    GetTimeLeft();
+    setInterval(function() {GetTimeLeft()}, 30000);
+}
+
+function GetTimeLeft() {
+    eel.get_shutdown_time_left()(time_left => {
+        if (time_left == -1) {
+            ui_input.value = "";
+        } else {
+            hours = Math.floor(time_left / 3600);
+            seconds = time_left % 3600;
+            minutes = Math.round(seconds / 60);
+            if (minutes == 60) {
+                hours += 1;
+                minutes = 0;
+            }
+            hours_text = hours.toString();
+            while (hours_text.length < 2) hours_text = "0" + hours_text;
+            minutes_text = minutes.toString();
+            while (minutes_text.length < 2) minutes_text = "0" + minutes_text;
+            ui_input.value = hours_text + ":" + minutes_text;
+        }
+    });
 }
